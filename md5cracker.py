f2c5e8bf90b32ea755fbe084d520f55690b4ada8
kingbiagio <35268328+kingbiagio@users.noreply.github.com>
2018-01-15 13:12:21 +0100

    Update md5cracker.py

--- md5cracker.py
+++ md5cracker.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python2.7
+# Author Biagio
 # Simple Hash md5 Cracker, run it with python 2.7
 # requirements termcolor
 #
@@ -13,12 +14,12 @@ import time
 
 print (colored("\n<<<< MD5 CRACKER >>>\n",'yellow')) # just banner
 counter=1
-user= raw_input(colored("[+] Insert User's hash You wish to Crack (or Otherwise Press Enter): ",'yellow'))
+user= raw_input(colored("[+] Insert User's hash You wish to Crack (or Otherwise Press Enter): ",'yellow'))#Banner
 pass_in= raw_input (colored("[+] Insert User's MD5 hash: ",'yellow'))
 pwfile= raw_input (colored("[+] Insert your path file name: ",'yellow'))
 
 print (colored("[*] Have Good Cracking ;)\n",'blue'))
-time.sleep(0.4) #Timeout
+time.sleep(0.4) #Timeout, change here different Timeout
 
 try:
 	pwfile= open(pwfile, "r")
