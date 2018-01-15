9ed32042ace89e054a087995168503bdde35b4a9
kingbiagio <35268328+kingbiagio@users.noreply.github.com>
2018-01-15 14:09:44 +0100

    Update and rename udp_scan.py to UDPscan.py

--- UDPscan.py
+++ UDPscan.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python
+#Author Biagio 
+#
+# install termcolor "pip install termcolor"
+#
 
 import logging
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
@@ -6,20 +10,21 @@ logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.all import *
 import time
 import sys
+from termcolor import*
 
 
 if len(sys.argv) != 4:
-    print "Usage - ./udp_scan.py [Target-IP] [First Port] [Last Port]"
-    print "Example - ./upd_scan.py 10.0.0.5 1 100"
-    print "Example will UDP port scan ports 1 through 100 on 10.0.0.5"
+    print (colored("       <---UDP SCANNER--->",'blue'))#simle banner
+    print (colored("./UDPscan.py [IP] [FirstPort] [LastPort]",'blue'))
     sys.exit()
+    
 else:
  ip = sys.argv[1]
  start = int(sys.argv[2])
  end = int(sys.argv[3])
 
  for port in range(start,end):
-    ans = sr1(IP(dst=ip)/UDP(dport=port),timeout=5,verbose =0)
+    ans = sr1(IP(dst=ip)/UDP(dport=port),timeout=5,verbose =0)#change here for different Timeout&Verbose
     time.sleep(1)
     if ans == None:
         print port
