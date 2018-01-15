d24c9656aa822ac5fc7e925b561d3fd8b88b2f4f
kingbiagio <35268328+kingbiagio@users.noreply.github.com>
2018-01-15 13:14:10 +0100

    Update pinger.py

--- pinger.py
+++ pinger.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python
 # Simple ICMP Discovery tool
-#
+# Author Biagio
 # sends ICMP Packet to subnet, to sollicits a response
-# Requirements Scapy, termcolor 
-# install these library pip install 
-
+# Requirements Scapy, termcolor pip install 
+# 
 
 import logging
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
@@ -13,7 +12,7 @@ from threading import*
 from termcolor import*
 
 if len(sys.argv) != 2:
-	print (colored("<--[ICMP DISCOVERY--]>",'red'))
+	print (colored("<--[ICMP DISCOVERY--]>",'red'))#Just banner
 	print (colored("Usage - ./pinger.py [IP/24 network address]\n",'red'))
 	sys.exit()
 
@@ -21,7 +20,7 @@ address = str(sys.argv[1])
 prefix = address.split('.')[0] + '.' + address.split('.')[1] + '.' + address.split('.')[2] + '.'
 
 for addr in range(1,254):  ## edit this for different Subnet in this example is \24
-	answer=sr1(IP(dst=prefix+str(addr))/ICMP(),timeout=1 , verbose=0)
+	answer=sr1(IP(dst=prefix+str(addr))/ICMP(),timeout=1 , verbose=0)#Change here for different Timeout
 	if answer == None:
 		pass
 	else:
