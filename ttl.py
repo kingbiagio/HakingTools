0488f8622905f11a0c07bf09dc2d92261de20b2b
kingbiagio <35268328+kingbiagio@users.noreply.github.com>
2018-01-15 13:23:34 +0100

    Update ttl.py

--- ttl.py
+++ ttl.py
@@ -1,10 +1,11 @@
-#!/usr/bin/python
+#!/usr/bin/python2.7
 #
-#Python version2
-#
+# Python version2
+# Requirements colored "pip install termcolor"
 # it analyze TTL value, to determinate if Os 
 # is Microsoft/windows or Unix
 #
+# CiscoRouterNetwork: 254 ttl
 # Microsoft/Windows:  128 ttl
 # Unix/Like:           64 ttl
 # 
@@ -17,7 +18,7 @@ import sys
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 
 if len(sys.argv) != 2:
-	print(colored("<TTL ANALYZER>",'blue'))
+	print(colored("<TTL ANALYZER>",'blue'))#just banner
 	print "Usage - ./ttl.py [Target]"
 	print "Usage - ./ttl.py 8.8.8.8."
 	sys.exit()
@@ -27,11 +28,11 @@ ans= sr1(IP(dst=str(ip))/ICMP(), timeout=1, verbose=0)
 if ans== None:
 	print(colored("[!] No response was returned Firewall blocks [ICMP] or Host is Down",'red'))
 
-elif int (ans[IP].ttl) >=254:
+elif int (ans[IP].ttl) >=254:# if OS is CiscoRouterNetwork
         print(colored("[+] Host is Cisco Router Network",'white'))
 
-elif int (ans[IP].ttl) <= 64:
+elif int (ans[IP].ttl) <= 64:#If OS Unix/linux
 	print (colored("[+] Host is Linux/Unix Os",'yellow'))
 
-else:
+else:                      #if OS is Windows
 	print(colored("[+] Host is Microsoft/Windows",'blue'))
