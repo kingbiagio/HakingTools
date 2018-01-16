417a02029ba0538dcb4bc598828ade1f7f046c44
kingbiagio <35268328+kingbiagio@users.noreply.github.com>
2018-01-16 16:27:28 +0100

    Update TTL.py

--- TTL.py
+++ TTL.py
@@ -24,9 +24,10 @@ if len(sys.argv) != 2:
 	print "Usage - ./TTL.py [Target IP]"
 	sys.exit()
 
-ip = sys.argv[1]
-ans= sr1(IP(dst=str(ip))/ICMP(), timeout=1, verbose=0)# Scapy variable set to [ICMP] protocol
-
+ip = sys.argv[1]              
+ans= sr1(IP(dst=str(ip))/ICMP(), timeout=1, verbose=0)# Scapy variable set to [ICMP] protocol,
+                                                      # Change here for different Timeout&Verbose
+	                         
 print (colored("<--TTL ANALYZER-->",'yellow'))
 if ans== None: # if none response was returned there is a Firewall behind or Host is Down
 	print(colored("[!] No response was returned Firewall blocks [ICMP] or Host is Down",'red'))
