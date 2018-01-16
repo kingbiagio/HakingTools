e5b5a997317e4977a7a6fe9fedd3284f9810db22
kingbiagio <35268328+kingbiagio@users.noreply.github.com>
2018-01-16 17:24:12 +0100

    Update ARP_disc.py

--- ARP_disc.py
+++ ARP_disc.py
@@ -14,13 +14,14 @@ from termcolor import*
 from threading import*
 
 if len(sys.argv) != 2:
-	print (colored("<--ARP DISCOVERY-->",'blue'))# Just Banner
 	print (colored("Usage - ./ARP_disc.py [interface]",'blue'))
 	sys.exit()
 
 interface = str(sys.argv[1]) # interface
 ip = subprocess.check_output("ifconfig " + interface + " | grep 'inet ' |  awk '{ print $2 }' | cut -d ':' -f2", shell=True).strip() # simple grep
 prefix = ip.split('.')[0] + '.' + ip.split('.')[1] + '.' + ip.split('.')[2] + '.'  
+
+print (colored("<--ARP DISCOVERY-->",'blue'))
 
 for addr in range(1,254):#change this for different Subnet, In this case is \24
 	answer=sr1(ARP(pdst=prefix+str(addr)),timeout=1,verbose=0) #variable created in scapy If you need change here for Timeout & Verbose
