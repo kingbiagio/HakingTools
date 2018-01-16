8e70885b30c9c993b62b0fd7de838ecc372fce2a
kingbiagio <35268328+kingbiagio@users.noreply.github.com>
2018-01-16 17:25:36 +0100

    Update ICMPsender.py

--- ICMPsender.py
+++ ICMPsender.py
@@ -12,12 +12,13 @@ from threading import*
 from termcolor import*
 
 if len(sys.argv) != 2:
-	print (colored("<--[ICMP DISCOVERY--]>",'red'))#Just banner
 	print (colored("Usage - ./ICMPsender.py [IP/24 network address]\n",'red'))
 	sys.exit()
 
 address = str(sys.argv[1])
 prefix = address.split('.')[0] + '.' + address.split('.')[1] + '.' + address.split('.')[2] + '.'
+
+print (colored("<--ICMP DISCOVERY-->",'red'))
 
 for addr in range(1,254):  ## edit this for different Subnet in this example is \24
 	answer=sr1(IP(dst=prefix+str(addr))/ICMP(),timeout=1 , verbose=0)#Change here for different Timeout
