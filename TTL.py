2e963ddc496678c19db0282ec3a3eeebdd49e602
kingbiagio <35268328+kingbiagio@users.noreply.github.com>
2018-01-16 16:24:46 +0100

    Update TTL.py

--- TTL.py
+++ TTL.py
@@ -20,13 +20,14 @@ import sys
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 
 if len(sys.argv) != 2:
-	print(colored("<--TTL ANALYZER-->",'blue'))#just banner
+        #just banner
 	print "Usage - ./TTL.py [Target IP]"
 	sys.exit()
 
 ip = sys.argv[1]
 ans= sr1(IP(dst=str(ip))/ICMP(), timeout=1, verbose=0)# Scapy variable set to [ICMP] protocol
 
+print (colored("<--TTL ANALYZER-->",'yellow'))
 if ans== None: # if none response was returned there is a Firewall behind or Host is Down
 	print(colored("[!] No response was returned Firewall blocks [ICMP] or Host is Down",'red'))
 
