fc4ad9086c6091a7aa95eed34796036a49d05c5c
kingbiagio <35268328+kingbiagio@users.noreply.github.com>
2018-01-16 17:13:10 +0100

    Update ACK_FW.py

--- ACK_FW.py
+++ ACK_FW.py
@@ -13,7 +13,6 @@ from scapy.all import *
 from threading import *
 
 if len(sys.argv) != 3:	
-	print (colored("<---FIREWALL DETECTIONER--->",'yellow'))
 	print (colored("Usage- ./ACK_FW.py [IP] [Port]", 'yellow'))
 	sys.exit()			
 
@@ -24,20 +23,22 @@ port = int (sys.argv[2])
 ACK_response = sr1(IP(dst= ip)/TCP(dport= port, flags='A'), timeout=1, verbose=0)
 SYN_response = sr1(IP(dst= ip)/TCP(dport= port, flags='S'), timeout=1, verbose=0)
 
+print (colored("<--FIREWALL DETECTION-->",'red'))
+
 # If ACK_response and SYN_response are NONE, the Port
 # is either unstatefully filtered or Host is Down
 if (ACK_response == None) and (SYN_response == None) :
-	print(colored("[+]Port is either Unstatefully Filtered, or Host is Down!",'red'))
+	print(colored("[+]Port is either Unstatefully Filtered, or Host is Down!",'red')), [port]
 
 #if one Response of Variable is None, Port is Filtered
 elif ((ACK_response == None) or (SYN_response == None)) and not ((ACK_response == None) and (SYN_response == None)):
-	print(colored("[!]Port is Filtered and Open",'red'))
+	print(colored("[!]Port is Filtered and Open",'red')), [port]
 
 elif int(SYN_response[TCP].flags) == 18:#Change different flag value
-	print(colored("[+]Port is Unfiltered and Open",'green'))
+	print(colored("[+]Port is Unfiltered and Open",'green')), [port]
 
 elif int(SYN_response[TCP].flags) == 20:#Change here for different Flag value
-	print(colored("[+]Port is Unfiltered and Close",'yellow'))
+	print(colored("[+]Port is Unfiltered and Close",'yellow')), [port]
 
 else:   
-	print(colored("[!]ERROR Unable to determine if the Port is Filtered",'red'))
+	print(colored("[!]ERROR Unable to determine if the Port is Filtered",'red')), [port]
