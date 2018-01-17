f55ba1246f9b17b49d71d81980078b71218e691a
kingbiagio <35268328+kingbiagio@users.noreply.github.com>
2018-01-17 11:49:13 -0500

    Update ICMPsender.py

--- ICMPsender.py
+++ ICMPsender.py
@@ -1,20 +1,26 @@
 #!/usr/bin/python
+#
+# Python 2
 # Simple ICMP Discovery tool
 # Author Biagio
+# Version 1.0
 # sends ICMP Packet ECHO REQUEST to subnet, it sollicits a response
 # Requirements Scapy, termcolor pip install 
 # 
+# Usage - ./ICMPsender.py [IP/24 CIDR]
 
 import logging
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.all import*
 from threading import*
 from termcolor import*
+import multiprocessing 
 
 if len(sys.argv) != 2:
-	print (colored("Usage - ./ICMPsender.py [IP/24 network address]\n",'red'))
+	print (colored("Usage - ./ICMPsender.py [IP/24 CIDR]\n",'red'))
 	sys.exit()
 
+#--global variable
 address = str(sys.argv[1])
 prefix = address.split('.')[0] + '.' + address.split('.')[1] + '.' + address.split('.')[2] + '.'
 
