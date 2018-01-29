cc62ba11234faeeaafe8c335475b66f2a3a749e5
kingbiagio <35268328+kingbiagio@users.noreply.github.com>
2018-01-29 15:55:57 +0100

    Update ACK_FW.py

--- ACK_FW.py
+++ ACK_FW.py
@@ -40,7 +40,7 @@ print "Author",Author,"\n"
 # is either unstatefully filtered or Host is Down
 try: 
     if (ACK_response == None) and (SYN_response == None): 
-        print(colored("[+]Port is either Unstatefully FILTERED",'red')),[port]
+        print(colored("[+]PORT IS EITHER UNSTATEFULLY FILTERED, OR HOST IS DOWN",'red')),[port]
         
         # if one Response of Variable is None, Port is Filtered 
     elif ((ACK_response == None) or (SYN_response == None)) and not ((ACK_response == None) and (SYN_response == None)): 
