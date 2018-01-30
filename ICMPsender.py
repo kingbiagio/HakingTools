af53562107be42abeef4bad3719570ec2ee2da9e
kingbiagio <35268328+kingbiagio@users.noreply.github.com>
2018-01-30 10:55:45 -0500

    Update ICMPsender.py

--- ICMPsender.py
+++ ICMPsender.py
@@ -2,7 +2,7 @@
 #
 # Python 2
 # Simple ICMP Discovery tool
-# Author SciancaBestia
+# Author ScianKaBestia
 # Version 1.1
 # sends ICMP Packet ECHO REQUEST to subnet, it sollicits a response
 # Requirements Scapy, termcolor pip install 
@@ -27,6 +27,7 @@ address = str(sys.argv[1])
 prefix = address.split('.')[0] + '.' + address.split('.')[1] + '.' + address.split('.')[2] + '.'
 
 print (colored("<--ICMP DISCOVERY-->",'red'))
+print "Author: ScianKaBestia"
 
 for addr in range(1,254): 
 	try: 
