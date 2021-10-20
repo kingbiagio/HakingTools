cafac7cfefec5bd04b03c08674782fbac3d49488
Mike Black <kingmikaelone@gmail.com>
2021-10-20 21:02:04 +0200

    Create arp_discovery.py

---
+++ arp_discovery.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+from scapy.config import conf
+conf.ipv6_enabled = False
+from scapy.all import *
+
+
+print("<--- Python ARP DISCOVERY --->")
+print("")
+print("USE - ./arp_discovery.py interface IP/CIDR")
+print("EX - ./arp_discovery.py eth1 192.168.0.0/24")
+print("")
+
+interface = sys.argv[1]
+ip_range = sys.argv[2]
+broadcastMac = "ff:ff:ff:ff:ff:ff"
+
+print("[+] Sending ARP packets..")
+print("")
+packet = Ether(dst=broadcastMac)/ARP(pdst = ip_range) 
+#conf.ipv6_enabled=False
+ans, unans = srp(packet, timeout =5, iface=interface, inter=0.1, verbose=False)
+
+for send,receive in ans:
+        print (receive.sprintf(r"%Ether.src% - %ARP.psrc%"))
