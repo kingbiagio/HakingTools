0a2fe3f0cfeb8477ad2bc4fef3ac4c3ab9be4bec
Mike Black <kingmikaelone@gmail.com>
2021-11-04 23:39:05 +0100

    Update Hash-Cracker.py

--- Hash-Cracker.py
+++ Hash-Cracker.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# sudo"apt install python3-pyfiglet
+# sudo apt install python3-pyfiglet
 
 import hashlib
 import pyfiglet
@@ -7,24 +7,34 @@ import sys
 
 ascii_banner = pyfiglet.figlet_format("HASH CRACKER", font="digital")
 print(ascii_banner)
-print("Hash Supported: MD5, SHA256\n")
+print("Hash Supported: NTLM, MD5, SHA-256\n")
 print("USE - ./Hash-Cracker.py [WORDLIST-PATH] [HASH]")
-print("EX - ./Hash-Cracker.py /usr/share/wordlist.txt 717d3e7b9278e122e65d6240c7ea9b81")
+print("EX - ./Hash-Cracker.py /usr/share/wordlists/rockyou.txt a332233d395c415a99c6b16a2348c129")
 print("")
 
 wordlist_location = sys.argv[1]
 hash_input = sys.argv[2]
 
 print("[*] Start Cracking...")
-with open(wordlist_location, 'r', encoding='latin-1') as file:
+with open(wordlist_location, 'r', encoding='latin-1') as file:  #encoding="ISO-8859-1"
 	for line in file.readlines():
+		
+		hash_NTLM = hashlib.new('md4', line.strip().encode('utf-16le')).digest()
+		
 		hash_md5 = hashlib.md5(line.strip().encode())
 		hashed_pass_md5 = hash_md5.hexdigest()
+		
 		hash_sha256 = hashlib.sha256(line.strip().encode())
 		hashed_pass_sha256 = hash_sha256.hexdigest()
+		
+		if binascii.hexlify(hash_NTLM).decode('utf-8') == hash_input:
+			print(("[+] NTLM Password is: " + line.strip()))
+			exit(0)
+		
 		if hashed_pass_md5 == hash_input:
 			print("[+] MD5 Password is: " + line.strip())
 			exit(0)
+			
 		if hashed_pass_sha256 == hash_input:
 			print("[+] SHA256 Password is: " + line.strip())
 			exit(0)
