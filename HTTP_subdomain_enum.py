67b559f37045235ed4a080946863583be1f88e86
Mike Black <kingmikaelone@gmail.com>
2021-11-22 11:43:31 +0100

    Create HTTP_subdomain_enum.py

---
+++ HTTP_subdomain_enum.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import sys, os, requests
+
+print("")
+print("--> SUBDOMAIN ENUMERATION <--")
+print("")
+print("USE - ./subdomain_enum.py [WORDLIST-PATH] [http or https] [IP/HOSTNAME]")
+print("EX -  ./subdomain_enum.py /usr/share/example.txt http google.com")
+print("EX -  ./subdomain_enum.py /usr/share/example.txt https google.com")
+print("")
+
+file = f"{sys.argv[1]}"
+path = os.getcwd() + file
+sub_list = open(file).read()
+subdoms = sub_list.splitlines()
+
+print("[+] STARTING ENUMERATION ON:",[sys.argv[3]])
+for sub in subdoms:
+    sub_domains = f"{sys.argv[2]}://{sub}.{sys.argv[3]}"
+
+    try:
+        requests.get(sub_domains)
+    except requests.ConnectionError:
+        pass
+    else:
+        print("[+]VALID DOMAIN: ",sub_domains)
