0acbcf1a3e5697f7f117960547a1deadb18ca990
Mike Black <kingmikaelone@gmail.com>
2021-11-22 17:23:05 +0100

    Create arp_sweep.sh

---
+++ arp_sweep.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [ "$#" -ne 1 ]; then
+echo "-> ARP Host Discovery <-"
+echo "Usage: sudo ./arp_sweep.sh [Network-ID]"
+echo "Example: sudo ./arp_sweep.sh 192.168.1.0"
+exit
+fi
+
+prefix=$(echo $1 | cut -d '.' -f 1-3)
+
+for addr in $(seq 1 254); do
+arping -w 3 -c 1 $prefix.$addr | grep "bytes from" &
+done
+exit
