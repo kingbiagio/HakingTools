06de01e8116030c94b7aeb49e8bbc6c0649cca85
Mike Black <kingmikaelone@gmail.com>
2026-04-22 15:00:33 +0200

    Refactor TCP scan script with port testing function
    Refactored TCP scan script to use a function for port testing and improved output messages.

--- tcp_scan.ps1
+++ tcp_scan.ps1
@@ -1,6 +1,29 @@
-echo "-> TCP Scan <-"
-echo "USE: .\tcp_scan.ps1 IP"
-echo ""
+Write-Output "-> TCP Scan <-"
+Write-Output "Usage: .\tcp_scan.ps1 <IP>"
+Write-Output ""
+
 $target = $args[0]
-#1..1024 | %{echo ((New-Object Net.Sockets.TcpClient).Connect(“$target”, $_)) “TCP Open port on - $_”} 2>$null
-20,21,22,25,53,80,88,110,135,139,143,389,443,445,1433,3306,3389,5985,5986,8080 | %{echo ((New-Object Net.Sockets.TcpClient).Connect(“$target”, $_)) “TCP Open port on - $_”} 2>$null
+
+# lista di porte
+$commonPorts = @(20,21,22,25,53,80,88,110,135,139,143,389,443,445,1433,3306,3389,5985,5986,8080)
+
+function Test-Port {
+    param (
+        [string]$ip,
+        [int]$port
+    )
+    try {
+        $tcpClient = New-Object System.Net.Sockets.TcpClient
+        $connectTask = $tcpClient.ConnectAsync($ip, $port)
+        if ($connectTask.Wait(1000) -and $tcpClient.Connected) {
+            Write-Output "TCP: $port is OPEN on $ip"
+        }
+        $tcpClient.Close()
+    } catch {
+        # Gestione errori
+    }
+}
+
+foreach ($port in $commonPorts) {
+    Test-Port -ip $target -port $port
+}
